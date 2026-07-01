b6ff75218b5d7fcfcfa2664124d255547f934f3d
buildwithshafikul <shafikul.18288@gmail.com>
2026-07-01 18:30:35 +0600

    test: update app name expectation
    Update the test case to match the rebranded application name "Hourly Salam & Time".

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -16,6 +16,6 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("Hourly Reminder Alarm", appName)
+    assertEquals("Hourly Salam & Time", appName)
   }
 }
